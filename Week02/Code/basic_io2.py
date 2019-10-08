2f146aa68e64757983a6f0b0af9cea956b20bd54
ph-u <51484056+ph-u@users.noreply.github.com>
2019-10-08 10:17:41 +0100

    sep basic_io.py into 3 component scripts

---
+++ Week02/Code/basic_io2.py
@@ -0,0 +1,12 @@
+##################################
+# FILE OUTPUT
+##################################
+# Save the elements of a list to a file
+list_to_save=range(100)
+
+f=open('../Sandbox/testout.txt','w')
+print("writing starts now")
+for i in list_to_save:
+	f.write(str(i) + '\n') ## Add a new line at the end
+print("job done")
+f.close()
