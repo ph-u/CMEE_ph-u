c1f8388af9838a0207a151e598706db5c5aa0f5e
ph-u <51484056+ph-u@users.noreply.github.com>
2019-10-09 11:18:10 +0100

    homework 20191009

--- Week02/Code/cfexercises1.py
+++ Week02/Code/cfexercises1.py
@@ -1,16 +1,39 @@
 #!/bin/env python3
 
+import sys
+import doctest
+
 # What does each of foo_x do?
 def foo_1(x):
+	""" test
+	>>> foo_1(4)
+	2.0
+	"""
 	return x ** .5
 
 def foo_2(x, y):
+	""" test
+	>>> foo_2(5,4)
+	5
+	>>> foo_2(4,10)
+	10
+	"""
 	if x > y:
 		return x
 	return y
 
 ## foo_3 breaks when z is the smallest number in the array
 def foo_3(x, y, z):
+	""" test
+	>>> foo_3(1,2,3)
+	[1, 2, 3]
+	>>> foo_3(2,1,3)
+	[1, 2, 3]
+	>>> foo_3(3,1,2)
+	[1, 2, 3]
+	>>> foo_3(3,2,1)
+	[1, 2, 3]
+	"""
 	if x > y:
 		tmp=y
 		y=x
@@ -22,19 +45,39 @@ def foo_3(x, y, z):
 	return [x, y, z]
 
 def foo_4(x):
+	""" test
+	>>> foo_4(1)
+	1
+	>>> foo_4(5)
+	120
+	"""
 	result=1
 	for i in range(1, x+1):
 		result=result * i
 	return result
 
 def foo_5(x): ## a recursive function that calculates the factorial of x
+	""" test
+	foo_5(1)
+	1
+	foo_5(5)
+	120
+	"""
 	if x == 1:
 		return 1
 	return x * foo_5(x-1)
 
 def foo_6(x): ## calculate the factorial of x in a different way
+	""" test
+	foo_6(1)
+	1
+	foo_6(5)
+	120
+	"""
 	facto=1
 	while x>=1:
 		facto=facto*x
 		x=x-1
 	return facto
+
+doctest.testmod()
--- Week02/Code/scope.py
+++ Week02/Code/scope.py
@@ -1,29 +1,90 @@
 #!/bin/env python3
 
-## Try this first
 
-_a_global=10
+## 1st
+_a_global = 10 # a global variable
+
+if _a_global >= 5:
+	_b_global = _a_global+5 # also a global variable
 
 def a_function():
-	_a_global=5
-	_a_local=4
-	print("Inside the function, the value is ", _a_global)
-	print("Inside the function, the value is ", _a_local)
+	_a_global = 5 # a local variable
+
+	if _a_global >=5:
+		_b_global = _a_global + 5 # also a local variable
+
+	_a_local = 4
+
+	print("Inside the function, the value of _a_global is ",_a_global)
+	print("Inside the function, the value of _b_global is ",_b_global)
+	print("Inside the function, the value of _a_local is ",_a_local)
+
 	return None
 
+print("1st block")
 a_function()
-print("Outside the function, the value is ", _a_global)
+print("Outside the function, the value of _a_global is ",_a_global)
+print("Outside the function, the value of _b_global is ",_b_global)
 
-## Now try this
 
-_a_global=10
+## 2nd
+_a_global = 10
+
+def a_function():
+	_a_local=4
+
+	print("Inside the function, the value _a_local is ",_a_local)
+	print("Inside the function, the value of _a_global is ",_a_global)
+	return None
+
+print("2nd block")
+a_function()
+print("Outside the function, the value of _a_global is ",_a_global)
+
+
+## 3rd
+_a_global = 10
+print("3rd block")
+print("Outside the function, the value of _a_global is ",_a_global)
+
 def a_function():
 	global _a_global
-	_a_global=5
-	_a_local=4
-	print("Inside the function, the value is ", _a_global)
-	print("Inside the function, the value is ", _a_local)
+	_a_global = 5
+	_a_local = 4
+
+	print("Inside the function, the value of _a_global is ",_a_global)
+	print("Inside the function, the value _a_local is ",_a_local)
+
 	return None
 
 a_function()
-print("Outside the function, the value is ", _a_global)
+print("Outside the function, the value of _a_global now is ",_a_global)
+
+
+## 4th
+print("4th block")
+def a_function():
+	_a_global = 10
+	def _a_function2():
+		global _a_global
+		_a_global = 20
+	print("Before calling a_function, value of _a_global is ",_a_global)
+
+	_a_function2()
+	print("After calling _a_function2, value of _a_global is ",_a_global)
+
+a_function()
+print("The value of a_global in main workspace / namespace is ",_a_global)
+
+## 5th
+_a_global=10
+print("5th block")
+def a_function():
+	def _a_function2():
+		global _a_global
+		_a_global=20
+	print("Before calling a_function, value of _a_global is ",_a_global)
+	_a_function2()
+	print("After calling _a_function2, value of _a_global is ",_a_global)
+a_function()
+print("The value of a_global in main workspace / namespace is ",_a_global)
