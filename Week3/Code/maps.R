950b25041693ae9a758338dd1f0626e5178e9146
ph-u <51484056+ph-u@users.noreply.github.com>
2019-10-16 23:28:30 +0100

    biases towards densely populated N-hemisphere European-based societies

--- Week3/Code/maps.R
+++ Week3/Code/maps.R
@@ -1,3 +1,13 @@
+#!/bin/env Rscript
+
+# Author: PokMan Ho pok.ho19@imperial.ac.uk
+# Script: maps.R
+# Desc: test GIS mapping in `ggplot2` pkg within R
+# Input: Rscript maps.R
+# Output: None
+# Arguments: 0
+# Date: Oct 2019
+
 ## key ref <https://eriqande.github.io/rep-res-web/lectures/making-maps-with-R.html>
 
 ## library
@@ -11,10 +21,10 @@ map(database = "world")
 
 ## map data on global map
 m<-map_data("world")
-pdf("../Sandbox/MappedMap.pdf",width = 15)
+# pdf("../Sandbox/MappedMap.pdf",width = 15)
 ggplot()+xlab("longitude")+ylab("latitude")+
   geom_map(data = m,map = m,aes(map_id=m$region,x=m$long,y=m$lat),fill="brown")+
   geom_point(aes(x=gpdd$long,y=gpdd$lat))
-dev.off()
+# dev.off()
 
 ## commit message: biases towards densely populated N-hemisphere European-based societies
