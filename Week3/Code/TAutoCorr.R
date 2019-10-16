8bbf125123133d80c2ca6c81d9ef0695ece43565
ph-u <51484056+ph-u@users.noreply.github.com>
2019-10-16 22:02:39 +0100

    homework

--- Week3/Code/TAutoCorr.R
+++ Week3/Code/TAutoCorr.R
@@ -16,7 +16,7 @@ b<-unlist(cor(ats,method = "spearman"))[1,2]
 ## cor coef (rand sample)
 dm<-1e4
 a<-rep(NA,dm);i<-1
-for(x in sample((dim(ats)[1]-1),dm,replace = T)){
+for(x in y<-sample((2:dim(ats)[1]),dm,replace = T)){
   a[i]<-unlist(cor(ats[(1:x),],method = "spearman"))[1,2]
   i<-i+1
   };rm(x,i,dm)
