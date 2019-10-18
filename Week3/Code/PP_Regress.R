4cf36f1dcfac147f6fb792329ec7cc12ec61a67a
ph-u <51484056+ph-u@users.noreply.github.com>
2019-10-19 00:48:09 +0100

    homework

--- Week3/Code/PP_Regress.R
+++ Week3/Code/PP_Regress.R
@@ -17,15 +17,12 @@ oo<-read.csv("../Data/EcolArchives-E089-51-D1.csv", header = T)
 ## plot & export
 yscale<-1e-6;for(i in 1:3){yscale[i+1]<-yscale[i]*(1e4)^i};rm(i)
 xscale<-1e-7;for(i in 1:2){xscale[i+1]<-xscale[i]*(1e4)^i};rm(i)
-pdf("../results/PP_Regress.pdf")
-ggplot(data = oo,aes(x=log(oo$Prey.mass),y=log(oo$Predator.mass),colour=oo$Predator.lifestage))+
+pdf("../results/PP_Regress.pdf");ggplot(data = oo,aes(x=oo$Prey.mass,y=oo$Predator.mass,colour=oo$Predator.lifestage))+
   geom_point(shape=3)+geom_smooth(method = "lm", fullrange=T)+theme_bw()+
   theme(legend.title = element_text(face = "bold"), legend.position = "bottom")+
-  scale_x_continuous(labels = function(x) format(x, scientific = TRUE), limits = c(1e-7,1e1), breaks = xscale)+
-  scale_y_continuous(labels = function(x) format(x, scientific = TRUE), limits = c(1e-6,1e6), breaks = yscale)+
   scale_color_discrete(name="Predator.lifestage")+
   facet_grid(oo$Type.of.feeding.interaction ~.)+
-  xlab("Prey Mass in grams")+ylab("Predator mass in grams")
-dev.off()
+  xlab("Prey Mass in grams")+ylab("Predator mass in grams")+
+  scale_x_continuous(labels=function(x) format(round(x), scientific = TRUE), limits = c(1e-7,1e1), trans = "log2")+scale_y_continuous(labels=function(x) format(round(x), scientific = TRUE), limits = c(1e-6,1e6), trans = "log2");dev.off()
 
 ## data info collect
