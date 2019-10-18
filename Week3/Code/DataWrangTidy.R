ff9e977fcb6d277c37bfe998b473a9cc43827dd1
ph-u <51484056+ph-u@users.noreply.github.com>
2019-10-18 08:29:00 +0100

    homework

--- Week3/Code/DataWrangTidy.R
+++ Week3/Code/DataWrangTidy.R
@@ -1,31 +1,34 @@
+library(dplyr)
+library(tidyr)
+
 MyData<-as.matrix(read.csv("../Data/PoundHillData.csv",header = F,stringsAsFactors = F))
 MyMetaData<-read.csv("../Data/PoundHillMetaData.csv",header = T,sep = ";",stringsAsFactors = F)
 class(MyData)
-head(MyData)
+tbl_df(MyData)## head(MyData)
 MyMetaData
 MyData[MyData==""]=0
-MyData<-t(MyData)
+MyData<-as.data.frame(t(MyData),stringsAsFactors = F)
 head(MyData)
 colnames(MyData)
 TempData<-as.data.frame(MyData[-1,],stringAsFactors=F)
-head(TempData)
+tbl_df(TempData)## head(TempData)
 colnames(TempData)<-MyData[1,] ## assign column names from original data
-head(TempData)
+tbl_df(TempData)## head(TempData)
 rownames(TempData)<-NULL
-head(TempData)
+tbl_df(TempData)## head(TempData)
 
-library(reshape2)
-MyWrangledData<-melt(TempData,id=c("Cultivation","Block","Plot","Quadrat"),variable.name = "Species",value.name = "Count")
-head(MyWrangledData);tail(MyWrangledData)
+# library(reshape2)
+# MyWrangledData<-melt(TempData,id=c("Cultivation","Block","Plot","Quadrat"),variable.name = "Species",value.name = "Count")
+MyWrangledData<-gather(TempData,"Species","Count",5:dim(TempData)[2])
+tbl_df(MyWrangledData);tbl_df(MyWrangledData[(dim(MyWrangledData)[1]-5):dim(MyWrangledData)[1],]);## head(MyWrangledData);tail(MyWrangledData)
 
 MyWrangledData[,"Cultivation"]<-as.factor(MyWrangledData[,"Cultication"])
 MyWrangledData[,"Block"]<-as.factor(MyWrangledData[,"Block"])
 MyWrangledData[,"Plot"]<-as.factor(MyWrangledData[,"Plot"])
 MyWrangledData[,"Quadrat"]<-as.factor(MyWrangledData[,"Quadrat"])
 MyWrangledData[,"Count"]<-as.integer(MyWrangledData[,"Count"])
-str(MyWrangledData)
+glimpse(MyWrangledData) ## str(MyWrangledData)
 
-library(dplyr)
 tbl_df(MyWrangledData) ## like head(), but nicer! <https://github.com/r-lib/vctrs/issues/487>
 glimpse(MyWrangledData) ## like str(), but nicer!
 filter(MyWrangledData,Count>100) ## like subset(), but nicer!
