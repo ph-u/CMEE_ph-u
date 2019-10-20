ae901d6aba97a5eeb162ca63ac926e710daa4712
ph-u <51484056+ph-u@users.noreply.github.com>
2019-10-20 17:13:43 +0100

    homework update

--- Week3/Code/PP_Lattice.R
+++ Week3/Code/PP_Lattice.R
@@ -27,11 +27,32 @@ densityplot(~log(oo$Prey.mass/oo$Predator.mass)|oo$Type.of.feeding.interaction,x
 dev.off()
 
 ## data description collect & export
-oo.0<-data.frame("mean"=rep(NA,3),"median"=rep(NA,3))
-j<-data.frame(oo$Predator.mass,oo$Prey.mass,oo$Predator.mass/oo$Prey.mass)
+a.0<-levels(oo$Type.of.feeding.interaction)
+oo.0<-data.frame(
+  "data"=c(rep("Predator.Mass.g",length(a.0)),
+           rep("Prey.Mass.g",length(a.0)),
+           rep("Prey/Predator.Mass",length(a.0)),
+           rep("log(Predator.Mass)",length(a.0)),
+           rep("log(Prey.Mass)",length(a.0)),
+           rep("log(Prey/Predator.Mass)",length(a.0))),
+  "FeedingType"=rep(NA,2*3*length(a.0)),
+  "mean"=rep(NA,2*3*length(a.0)),
+  "median"=rep(NA,2*3*length(a.0)))
+oo.0[,2]<-a.0
+oo$ratio<-oo$Prey.mass/oo$Predator.mass
 for(i in 1:dim(oo.0)[1]){
-  oo.0[i,1]<-mean(j[,i])
-  oo.0[i,2]<-median(j[,i])
-};rm(i,j)
-row.names(oo.0)=c("Predator.Mass.g","Prey.Mass.g","Predator.Mass/Prey.Mass")
+  ## determine which input source is used
+  if(length(grep("y/P",oo.0[i,1]))>0){j.0<-16
+  }else if(length(grep("Prey",oo.0[i,1]))>0){j.0<-13
+  }else{j.0<-9}
+  
+  ## calculation
+  if(length(grep("log",oo.0[i,1]))>0){
+    oo.0[i,3]<-mean(log(oo[which(oo$Type.of.feeding.interaction==oo.0$FeedingType[i]),j.0]))
+    oo.0[i,4]<-median(log(oo[which(oo$Type.of.feeding.interaction==oo.0$FeedingType[i]),j.0]))
+  }else{
+    oo.0[i,3]<-mean(oo[which(oo$Type.of.feeding.interaction==oo.0$FeedingType[i]),j.0])
+    oo.0[i,4]<-median(oo[which(oo$Type.of.feeding.interaction==oo.0$FeedingType[i]),j.0])
+  }
+};rm(i)
 write.csv(oo.0,"../Results/PP_Results.csv", quote = F, row.names=T)
