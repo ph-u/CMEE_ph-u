1f9e0ed0230cf090d482ebdca578ee92e4287016
ph-u <51484056+ph-u@users.noreply.github.com>
2019-10-25 18:42:01 +0100

    prep

---
+++ MiniProject/code/run_MiniProject.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Author: PokMan Ho pok.ho19@imperial.ac.uk
+# Script: run_MiniProject.sh
+# Desc: Master script file reproducing the MiniProject
+# Input: ./run_MiniProject.sh
+# Output: 1. `pdf` output in main project subdirectory; 2. all result graphs in `results` subdirectory
+# Arguments: 0
+# Date: Oct 2019
+
+
