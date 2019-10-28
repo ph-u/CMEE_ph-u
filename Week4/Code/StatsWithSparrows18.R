27cd62b06208320ea82d8803f2f83752a201e1d7
ph-u <51484056+ph-u@users.noreply.github.com>
2019-10-28 11:40:44 +0000

    classwork

---
+++ Week4/Code/StatsWithSparrows18.R
@@ -0,0 +1,24 @@
+#!/bin/env Rscript
+
+# Author: PokMan Ho pok.ho19@imperial.ac.uk
+# Script: StatsWithSparrows18.R
+# Desc: minimal R function with two in-script tests
+# Input: none -- run in R console line-by-line
+# Output: R terminal output
+# Arguments: 0
+# Date: Oct 2019
+
+a<-read.table("../Data/ObserverRepeatability.txt", header = T)
+library(dplyr)
+a %>% group_by(StudentID) %>% summarise(count=length(StudentID))
+a %>% group_by(StudentID) %>% summarise(count=length(StudentID)) %>% summarise(length(StudentID))
+a %>% group_by(StudentID) %>% summarise(count=length(StudentID)) %>% summarise(sum(count))
+length(a$StudentID)
+a %>% group_by(StudentID) %>% summarise(count=length(StudentID)) %>% summarise(sum(count^2))
+mod<-lm(a$Tarsus~a$StudentID)
+mod<-lm(a$Tarsus~a$Leg+a$Handedness+a$StudentID)
+anova(mod)
+library(lme4)
+lmm<-lmer(a$Tarsus~a$Leg+a$Handedness+(1|a$StudentID))
+summary(lmm)
+var(a$Tarsus)
