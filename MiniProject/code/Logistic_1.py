a7a32daa45ddb7d4011a4009bccb8a06de2a8336
ph-u <51484056+ph-u@users.noreply.github.com>
2019-11-03 01:29:12 +0000

    Update Logistic_1.py

--- MiniProject/code/Logistic_1.py
+++ MiniProject/code/Logistic_1.py
@@ -8,5 +8,28 @@
 # Arguments: 0
 # Date: Nov 2019
 
-## lib
+"""model-fitting for LogisticGrowthMetaData.csv"""
 
+__appname__='Logistic_1.py'
+__author__='PokMan HO (pok.ho19@imperial.ac.uk)'
+__version__='0.0.1'
+__license__='License for this code / program'
+
+## lib
+import sys
+import os
+
+## logistic equations
+def log0(N0, r, t, K):
+    """traditional Logistic equation"""
+    a=N0*K*exp(r*t)/(K+N0*(exp(r*t)-1))
+    return(a)
+def Gom(Nmx, Nmn, rmx, ld, t):
+    """modified Gompertz model"""
+    a.0=log(Nmx/Nmn)
+    a.1=a.0*exp(-exp(rmx*exp/a.0*(ld-t)+1))
+    return(a.1)
+def Bar(args):
+    """Baranyi model"""
+
+## raw data
