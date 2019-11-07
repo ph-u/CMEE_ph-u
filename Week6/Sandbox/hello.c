b7a423054e2a720630dcdfec116d8e71543ebdc8
ph-u <51484056+ph-u@users.noreply.github.com>
2019-11-07 11:55:39 +0000

    first C taste :+1: :accept: :heart:

---
+++ Week6/Sandbox/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+// <https://stackoverflow.com/questions/32337643/how-to-run-c-program-on-mac-os-x-using-terminal>
+// clang hello.c -o hi;./hi
+
+// printf() displays the string inside quotation
+printf("Hello World!\n");
+return 0;
+}
