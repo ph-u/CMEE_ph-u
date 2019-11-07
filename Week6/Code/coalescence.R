bd18685e7693f35370213d1067d32c1a45b8a4f8
ph-u <51484056+ph-u@users.noreply.github.com>
2019-11-07 14:25:22 +0000

    restructuring Week 5

---
+++ Week6/Code/coalescence.R
@@ -0,0 +1,13 @@
+#!/bin/env R
+
+# Author: PokMan Ho pok.ho19@imperial.ac.uk
+# Script: coalescence.R
+# Desc: classwork program based on killer whale data files
+# Input: Rscript coalescence.R
+# Output: R terminal output
+# Arguments: 0
+# Date: Nov 2019
+
+## lib
+
+## raw
