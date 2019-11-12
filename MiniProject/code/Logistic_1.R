2cb4d2f69a73714d19733a7bc7304f54e13c3a54
ph-u <51484056+ph-u@users.noreply.github.com>
2019-11-12 09:15:55 +0000

    miniproject partial

--- MiniProject/code/Logistic_1.R
+++ MiniProject/code/Logistic_1.R
@@ -21,26 +21,18 @@ r.b<-max(a.0[which(a.0$cluster==2),2])-r.m*max(a.0[which(a.0$cluster==2),3])
 r.x<--r.b/r.m
 
 ## logistic equations: y=Nt x=exp(t)
-func_log0<-function(N0=as.numeric(a.1[20,2]),
-                    K=as.numeric(a.1[22,2]),
-                    r=r.m, t){
+func_log0<-function(N0, K, r, t){
   ## traditional Logistic equation: y~x
   Nt=N0*K*exp(r*t)/(K+N0*(exp(r*t)-1))
   return(Nt)}
 
-func_gom<-function(N0=as.numeric(a.1[20,2]),
-                   K=as.numeric(a.1[22,2]),
-                   r=r.m, t,
-                   ld=r.x){
+func_gom<-function(N0, K, r, t, ld){
   ## modified Gompertz model, initial 
   A=log(K/N0)
-  Nt=exp(A*exp(-exp(r*exp(1)/A*(ld-t)+1)))
+  Nt=A*exp(-exp(r*exp(1)/A*(ld-t)+1))
   return(Nt)}
 
-func_bar<-function(N0=as.numeric(a.1[20,2]),
-                   K=as.numeric(a.1[22,2]),
-                   r=r.m, t,
-                   tlag=max(a.0[which(a.0$cluster==1),3])){
+func_bar<-function(N0, K, r, t, tlag=max(a.0[which(a.0$cluster==1),3])){
   ## Baranyi model
   h0=1/(exp(tlag*r)-1)
   At=t+1/r*log((exp(-r*t)+h0)/(1+h0))
@@ -60,4 +52,13 @@ func_buc<-function(N0=as.numeric(a.1[20,2]),
 
 ## can't run model because data is too good? <https://stats.stackexchange.com/questions/13053/singular-gradient-error-in-nls-with-correct-starting-values>
 ## can't run model because models are too board / too specific? <https://stackoverflow.com/questions/35409099/r-minpack-lmnls-lm-failed-with-good-results>
-nlsLM(Popn_Change ~ func_log0(t=a.0$Time.hr), data = a.0, start = list(N0=as.numeric(a.1[20,2]),K=as.numeric(a.1[21,2]),r=r.m))
+nls_log0<-nlsLM(Popn_Change ~ func_log0(N0, K, r, t=a.0$Time.hr), data = a.0, start = list(N0=as.numeric(a.1[20,2]),K=as.numeric(a.1[21,2]),r=r.m), lower = c(min(a.0[which(a.0[,5]==1),4]),min(a.0[which(a.0[,5]==3),4]),r.m-1), upper = c(max(a.0[which(a.0[,5]==1),4]),max(a.0[which(a.0[,5]==3),4]),r.m+1))
+
+nls_gom<-nlsLM(Popn_Change ~ func_gom(N0, K, r, t=a.0$Time.hr, ld), data = a.0, start = list(N0=as.numeric(a.1[20,2]),K=as.numeric(a.1[21,2]),r=r.m,ld=r.x), lower = c(min(a.0[which(a.0[,5]==1),4]),min(a.0[which(a.0[,5]==3),4]),r.m-1,r.x-1), upper = c(max(a.0[which(a.0[,5]==1),4]),max(a.0[which(a.0[,5]==3),4]),r.m+1,r.x+1))
+## tests
+for(i in 1:length(a.0[,3])){j<-a.0[i,3];print(paste(i,j,func_gom(N0=as.numeric(a.1[20,2]),K=as.numeric(a.1[21,2]),r=r.m, t=a.0[j,3],ld=r.x)))};rm(i,j)
+plot(func_gom(N0=as.numeric(a.1[20,2]),K=as.numeric(a.1[21,2]),r=r.m,ld=r.x, t=a.0[,3])~a.0[,3])
+
+nls_bar<-nlsLM(Popn_Change ~ func_log0(N0, K, r, t=a.0$Time.hr), data = a.0, start = list(N0=as.numeric(a.1[20,2]),K=as.numeric(a.1[21,2]),r=r.m), lower = c(min(a.0[which(a.0[,5]==1),4]),min(a.0[which(a.0[,5]==3),4]),r.m-1), upper = c(max(a.0[which(a.0[,5]==1),4]),max(a.0[which(a.0[,5]==3),4]),r.m+1))
+
+nls_buc<-nlsLM(Popn_Change ~ func_log0(N0, K, r, t=a.0$Time.hr), data = a.0, start = list(N0=as.numeric(a.1[20,2]),K=as.numeric(a.1[21,2]),r=r.m), lower = c(min(a.0[which(a.0[,5]==1),4]),min(a.0[which(a.0[,5]==3),4]),r.m-1), upper = c(max(a.0[which(a.0[,5]==1),4]),max(a.0[which(a.0[,5]==3),4]),r.m+1))
