dc228d8c739145f28f89a326dfcb709f83cabfb7
ph-u <51484056+ph-u@users.noreply.github.com>
2019-11-15 14:32:19 +0000

    partial classwork

---
+++ Week7/Code/run_fmr_R.py
@@ -0,0 +1,18 @@
+#!/bin/env python3
+
+# Author: PokMan Ho pok.ho19@imperial.ac.uk
+# Script: run_fmr_R.py
+# Desc: call fmr.R from python3 terminal environment
+# Input: python3 run_fmr_R.py
+# Output: terminal output
+# Arguments: 0
+# Date: Nov 2019
+
+
+"""call fmr.R from python3 terminal environment"""
+
+__appname__="run_fmr_R.py"
+__author__="PMH"
+__version__="0.0.1"
+__license__="None"
+
