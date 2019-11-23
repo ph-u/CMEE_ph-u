4ca49c3b77dd9c78488aec3d2d99e8454ab375f2
ph-u <51484056+ph-u@users.noreply.github.com>
2019-11-23 12:12:53 +0000

    partial workflow plan

---
+++ MiniProject/code/Log_1_c.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Author: PokMan Ho pok.ho19@imperial.ac.uk
+# Script: Log_1_c.sh
+# Desc: commander script for Log_1_s*.R slaves
+# Input: bash Log_1_c.sh <Dataset Num> <Iterations>
+# Output: none
+# Arguments: 2
+# Date: Nov 2019
+
+if [ -z $1 ];then
+    echo -e "No input"
+    exit
+fi
+
+Rscript Log_1_s1.R $1
+Rscript Log_1_s2.R $1 $2
+exit
--- MiniProject/code/Log_2.sh
+++ MiniProject/code/Log_2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Author: PokMan Ho pok.ho19@imperial.ac.uk
-# Script: Log_1.sh
-# Desc: control script for Log_1_c.R commander script
-# Input: bash Log_1.sh
-# Output: none
+# Script: Log_2.sh
+# Desc: collector for slave script results
+# Input: bash Log_2.sh
+# Output: combined result of best-fit parameters
 # Arguments: 0
 # Date: Nov 2019
 
