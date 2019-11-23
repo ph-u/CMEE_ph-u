f31f72d11e6e1081c8301879fda4e12cd05b4a19
ph-u <51484056+ph-u@users.noreply.github.com>
2019-11-23 20:28:59 +0000

    parallel miniproject run complete

--- MiniProject/code/Log_1.sh
+++ MiniProject/code/Log_1.sh
@@ -8,7 +8,29 @@
 # Arguments: 0
 # Date: Nov 2019
 
-a0=`wc -l Log_Uq.txt|tr -s " "|cut -f 2 -d " "`
-a0=`echo $((${a0} -1))`
+a0=`wc -l ../data/Log_Uq.txt|tr -s " "|cut -f 2 -d " "` ## get number of unique dataset for parallel processing
 
+for i in `seq 1 $((${a0} -1))`;do ## parallel data subset processing
+	nohup ./Log_1_c.sh ${i} 1e2 &
+done
 
+sleep 20 ## allow some time to run slave scripts before loading CPU for checking progress
+
+## hault control script for intermediate data collection
+while [ $((`ps aux|grep slave|grep R|wc -l`)) -gt 0 ];do
+	sleep 10
+done
+
+## collect intermediate result
+touch ../data/Log_tt_data.txt
+touch ../data/Log_tt_para.txt
+for i in `seq 1 $((${a0} -1))`;do ## parallel data subset processing
+	cat ../data/Log_${i}_data.txt >> ../data/Log_tt_data.txt
+	cat ../data/Log_${i}_para.txt >> ../data/Log_tt_para.txt
+done
+
+for i in `ls ../data/*|grep "Log_[0-9]*_[dp]a[tr]a.txt"`;do
+	rm ${i}
+done
+rm nohup*
+exit
