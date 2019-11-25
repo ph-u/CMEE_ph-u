f65ab99af2a87992b1e739cc8689722725a30c93
ph-u <51484056+ph-u@users.noreply.github.com>
2019-11-25 18:03:24 +0000

    classwork

--- Week8/Code/pokho_HPC_2019_main.R
+++ Week8/Code/pokho_HPC_2019_main.R
@@ -40,54 +40,111 @@ choose_two <- function(max_value){
 
 # Question 5
 neutral_step <- function(community){
-  
+  a<-choose_two(length(community))
+  if(runif(1) < .5){community[a[1]] <- community[a[2]]}else{community[a[2]] <- community[a[1]]}
+  return(community)
 }
 
 # Question 6
 neutral_generation <- function(community){
-  
+  n0<-length(community)
+  if(n0%%2!=0 & runif(1)<.5){n1<-floor(n0/2)+n0%%2}else{n1<-floor(n0/2)}
+  for(i in 1:n1){
+    community<-neutral_step(community)
+  }
+  return(community)
 }
 
 # Question 7
 neutral_time_series <- function(community,duration)  {
-  
+  sp_rich<-c()
+  for(i in 1:duration){
+    community<-neutral_generation(community)
+    sp_rich<-c(sp_rich,species_richness(community))
+  }
+  return(sp_rich)
 }
 
 # Question 8
 question_8 <- function() {
-  # clear any existing graphs and plot your graph within the R window
-  return("type your written answer here")
+  graphics.off() # clear any existing graphs and plot your graph within the R window
+  n0<-200
+  cat("num of generation: ")
+  repeat{
+    if(n0%%10==0){cat(paste0(n0,"; "))}
+    a<-neutral_time_series(init_community_max(100), n0)
+    if(min(a)==1){break} ## each run can never go up in species richness, assume no speciation (think biologically -- Francis Windram)
+    n0<-n0+1
+  }
+  cat("\n")
+  plot(a~seq(1:n0), pch=3, xlab="generation", ylab = "species richness", type="l", col="red") ## plot species richness 
+  # lines(rep(1,n0)~seq(1:n0), add=T)
+  
+  return(paste0("species richness = ",min(a)," at ",min(which(a==min(a)))," generations"))
 }
 
 # Question 9
 neutral_step_speciation <- function(community,speciation_rate)  {
-  
+  a0<-choose_two(length(community))
+  a1<-runif(2)
+  ## section allow spp reappearing
+  # a2<-seq(1:length(community)^2)
+  # a3<-a2!=community
+  # a3<-sample(a2[a3==T],1)
+  a3<-max(community)+1 ## not allow spp reappearing
+  if(a1[2]<.5){i0<-1}else{i0<-2}
+  if(a1[1]<speciation_rate){i1<-a3}else{i1<-community[a0[i0%%length(a0)+1]]}
+  community[a0[i0]]<-i1
+  return(community)
 }
 
 # Question 10
 neutral_generation_speciation <- function(community,speciation_rate)  {
-  
+  n0<-length(community)
+  if(n0%%2!=0 & runif(1)<.5){n1<-floor(n0/2)+n0%%2}else{n1<-floor(n0/2)}
+  for(i in 1:n1){
+    community<-neutral_step_speciation(community,speciation_rate)
+  }
+  return(community)
 }
 
 # Question 11
 neutral_time_series_speciation <- function(community,speciation_rate,duration)  {
-  
+  sp_rich<-c()
+  for(i in 1:duration){
+    community<-neutral_generation_speciation(community,speciation_rate)
+    sp_rich<-c(sp_rich,species_richness(community))
+  }
+  return(sp_rich)
 }
 
 # Question 12
 question_12 <- function()  {
-  # clear any existing graphs and plot your graph within the R window
-  return("type your written answer here")
+  graphics.off() # clear any existing graphs and plot your graph within the R window
+  n0<-200
+  sp_rate<-.1
+  a0<-neutral_time_series_speciation(init_community_max(100),sp_rate, n0)
+  a1<-neutral_time_series_speciation(init_community_min(100),sp_rate, n0)
+  b0<-abs(a0-a1)
+  
+  plot(a0~seq(1:n0), pch=3, xlab="generation", ylab = "species richness", type="l", col="red") ## plot species richness 
+  lines(a1~seq(1:n0), pch=3, xlab="generation", ylab = "species richness", type="l", col="blue", add=T)
+  
+  # return(paste0("species richness min difference at ",b0," at ",which(abs(a0-a1)==b0)," generations"))
+  return(cat(paste0("Throughout ",n0," generations,\nIQR of species richness difference is ",IQR(b0),", from ",fivenum(b0)[2]," to ",fivenum(b0)[4],";\nIQR of high species diversity goup is from ",fivenum(a0)[2]," to ",fivenum(a0)[4],";\nIQR of low species diversity goup is from ",fivenum(a1)[2]," to ",fivenum(a1)[4],"\nReason of this pattern is due to the introduction of speciation with rate ",sp_rate,", which new mutations are hard to get rid of but diversity is easy to be reduced\nSo initial states have no observable effect on the final result\n")))
 }
 
 # Question 13
 species_abundance <- function(community)  {
-  
+  return(rev(sort(as.data.frame(table(community))[,2])))
 }
 
 # Question 14
 octaves <- function(abundance_vector) {
-  
+  a<-table(floor(log2(abundance_vector)))
+  a0<-data.frame(seq(1,max(names(a))),0)
+  for(i in 1:dim(a0)[1]){a0[i,2]<-a[which(names(a)==a0[i,1])]}
+  return(a0[,2])
 }
 
 # Question 15
@@ -133,7 +190,7 @@ chaos_game <- function()  {
 
 # Question 24
 turtle <- function(start_position, direction, length)  {
-
+  
   return() # you should return your endpoint here.
 }
 
