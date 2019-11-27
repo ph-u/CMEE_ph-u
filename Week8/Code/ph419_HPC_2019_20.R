c7ad758409e78b2ea58a30c34c8186c84ece8956
ph-u <51484056+ph-u@users.noreply.github.com>
2019-11-27 00:57:34 +0000

    Create ph419_HPC_2019_20.R

---
+++ Week8/Code/ph419_HPC_2019_20.R
@@ -0,0 +1,9 @@
+#!/bin/env R
+
+# Author: PokMan Ho pok.ho19@imperial.ac.uk
+# Script: ph419_HPC_2019_18.R
+# Desc: HPC homework Q18
+# Input: Rscript ph419_HPC_2019_18.R
+# Output: none
+# Arguments: 0
+# Date: Nov 2019
