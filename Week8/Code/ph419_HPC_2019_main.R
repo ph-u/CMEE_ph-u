b37d53d27f7cfd4b3d56481a6808218806bcc33a
ph-u <51484056+ph-u@users.noreply.github.com>
2019-12-06 09:32:10 +0000

    homework hpc

--- Week8/Code/ph419_HPC_2019_main.R
+++ Week8/Code/ph419_HPC_2019_main.R
@@ -341,31 +341,46 @@ draw_tree <- function()  {
 }
 
 # Question 29
-fern <- function(start_position=c(.5,0), direction=90*2*pi/360, length=.3, LR=0, LR1=0)  {
+fern <- function(start_position=c(.5,0), direction=90*2*pi/360, length=.1, LR=1)  {
   a<-turtle(start_position,direction,length)
-  suppressWarnings(lines(x=c(start_position[1],a[1]), y=c(start_position[2],a[2]), add=T))
-  dirr<-direction+ifelse(LR==0,pi/4,0)
-  lenn<-length*ifelse(LR==0,.38,.87)
-  if(length>1e-2){
-    fern(start_position = a, direction = dirr, length = lenn, LR=0, LR1=0)
-    fern(start_position = a, direction = dirr, length = lenn, LR=0, LR1=1)
-    fern(start_position = a, direction = dirr, length = lenn, LR=1, LR1=0)
-    fern(start_position = a, direction = dirr, length = lenn, LR=1, LR1=1)
-  }
+  colouring<-c(rgb(0,.7,.1,1),rgb(.5,.5,.2,1),rgb(1,0,0,1)) ## leaves, branches, tips
+  # colouring<-c(rgb(.5,.5,1,1),rgb(.5,.5,.2,1),rgb(1,.3,1,.6)) ## pink variation
+  ccol<-ifelse(length>5e-3,colouring[2],ifelse(length>1.2e-3,colouring[1],colouring[3]))
+  suppressWarnings(lines(x=c(start_position[1],a[1]), y=c(start_position[2],a[2]), add=T, col=ccol))
+  
+  ## parameters mod
+  d<-direction+ifelse(LR<1,pi/4,ifelse(LR>1,-pi/4,0))
+  L<-length*ifelse(LR==1,.87,.38)
+  
+  if(L > 1e-3){for(i in 0:2){fern(a,d,L,i)}}
 }
 draw_fern <- function()  {
   graphics.off() # clear any existing graphs and plot your graph within the R window
   plot.new()
   fern()
-  # for(i in 0:1){for(j in 0:1){fern(LR=i, LR1=j)}}
 }
 
 # Question 30
-fern2 <- function(start_position, direction, length)  {
+fern2 <- function(start_position=c(.5,0), direction=90*2*pi/360, length=.1, LR=1, LR1=0)  {
+  a<-turtle(start_position,direction,ifelse(LR1==0,length*.2,length))
+  # a<-turtle(start_position,direction,length)
+  colouring<-c(rgb(0,.7,.1,1),rgb(.5,.5,.2,1),rgb(1,0,0,1)) ## leaves, branches, tips
+  # colouring<-c(rgb(.5,.5,1,1),rgb(.5,.5,.2,1),rgb(1,.3,1,.6)) ## pink variation
+  ccol<-ifelse(length>5e-3,colouring[2],ifelse(length>1.2e-3,colouring[1],colouring[3]))
+  suppressWarnings(lines(x=c(start_position[1],a[1]), y=c(start_position[2],a[2]), add=T, col=ccol))
   
+  ## parameters mod
+  d<-direction+ ## core direction upwards
+    ifelse(LR<1,pi/4,0)+ ## change angle of branches
+    ifelse(LR1%%2==0 & LR!=1, -pi/2,0) ## flip half of branches
+  L<-length*ifelse(LR==1,.88,.38)
+  
+  if(L > 1e-3){for(i in 0:1){fern2(a,d,L,i,LR1+1)}}
 }
 draw_fern2 <- function()  {
-  # clear any existing graphs and plot your graph within the R window
+  graphics.off() # clear any existing graphs and plot your graph within the R window
+  plot.new()
+  fern2()
 }
 
 # Challenge questions - these are optional, substantially harder, and a maximum of 16% is available for doing them.  
