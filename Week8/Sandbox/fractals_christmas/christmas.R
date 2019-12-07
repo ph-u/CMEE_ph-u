14e8c1a931426a50b12b3c90e16b2c7216f14991
ph-u <51484056+ph-u@users.noreply.github.com>
2019-12-07 22:17:57 +0000

    Update christmas.R

--- Week8/Sandbox/fractals_christmas/christmas.R
+++ Week8/Sandbox/fractals_christmas/christmas.R
@@ -6,8 +6,9 @@ turtle <- function(start_position=c(0,0), direction=.03, length=.1)  {
   return(a) # you should return your endpoint here.
 }
 
-fern <- function(start_position=c(.5,0), direction=90*2*pi/360, length=.1, LR=1, pp, pp1=0)  {
+fern <- function(start_position=c(.5,0), direction=90*2*pi/360, length=.1, LR=1, pp, pp1=0, pp2)  {
   a<-turtle(start_position,direction,ifelse(pp1==0, length*.1,length))
+		  if(pp2==0){
 		  if(pp==1 | pp==5){
 				  colouring<-c(rgb(0,.7,.1,1),rgb(.5,.5,.2,1),rgb(1,0,0,1)) ## leaves, branches, tips
 		  }else if(pp==2){
@@ -16,7 +17,14 @@ fern <- function(start_position=c(.5,0), direction=90*2*pi/360, length=.1, LR=1,
 				  colouring<-c(rgb(0,.7,1,1),rgb(.6,0,0,1),rgb(1,.5,0,1)) ## orange-themed
 		  }else if(pp==4){
 				  colouring<-c(rgb(.5,.5,1,1),rgb(.5,.5,.2,1),rgb(1,.3,1,1)) ## pink-themed
-		  }
+		  }}else{
+		  if(pp==1 | pp==4){
+				  colouring<-c(rgb(0,.7,.1,1),rgb(.5,.5,.2,1),rgb(1,0,0,1)) ## leaves, branches, tips
+		  }else if(pp==2){
+				  colouring<-c(rgb(0,.7,.1,1),rgb(.5,.5,.2,1),rgb(0,0,1,1)) ## leaves, branches, tips
+		  }else if(pp==3){
+				  colouring<-c(rgb(0,.7,.1,1),rgb(.5,.5,.2,1),rgb(1,.5,0,1)) ## leaves, branches, tips
+		  }}
   # colouring<-c(rgb(.5,.5,1,1),rgb(.5,.5,.2,1),rgb(1,.3,1,.6)) ## pink variation
   ccol<-ifelse(length>5e-3,colouring[2],ifelse(length>1.2e-3,colouring[1],colouring[3]))
   suppressWarnings(lines(x=c(start_position[1],a[1]), y=c(start_position[2],a[2]), add=T, col=ccol))
@@ -28,12 +36,14 @@ fern <- function(start_position=c(.5,0), direction=90*2*pi/360, length=.1, LR=1,
   if(L > 1e-3){for(i in 0:2){fern(a,d,L,i,pp,1)}}
 }
 
-draw_fern <- function()  {
+draw_fern <- function(k)  {
   graphics.off() # clear any existing graphs and plot your graph within the R window
   plot.new()
   for(i in 1:5){
-		  fern(pp=i)
+		  fern(pp=i,pp2=k)
   }
 }
 
-draw_fern()
+for(i in 0:1){
+	draw_fern(i)
+}
