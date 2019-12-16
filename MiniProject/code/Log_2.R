0d3f49fce625079eeba0e036240bd7d8022868fa
ph-u <51484056+ph-u@users.noreply.github.com>
2019-12-16 00:38:24 +0000

    check-point on statistics aspect

--- MiniProject/code/Log_2.R
+++ MiniProject/code/Log_2.R
@@ -39,7 +39,7 @@ for(i in 1:dim(ref)[1]){ ## run through ref table
 kt<-kruskal.test(ref[,3]~ref[,2]) ## non-sig result of difference
 
 pdf("../results/barplot_BestModel.pdf", width = 10)
-barplot(ref[,3]~ref[,2], ylab = paste0("best-model occurrence, each bar max at ",max(a[,2])), xlab = "Model", ylim=c(0,max(a[,2])))  ## plot best model occurrence
+barplot(ref[,3]~ref[,2], ylab = paste0("best-model occurrence, each bar max at ",max(a[,2])), xlab = "Model", ylim=c(0,max(a[,2])), cex.lab=1.5, cex.axis=1.5)  ## plot best model occurrence
 #text(x=dim(ref)[1]/2,y=max(a.md[,2])*.8, labels = paste0("Kruskal-Wallis Test\nX^2 = ",kt$statistic,"\ndf = ",kt$parameter,"\np-val = ",round(kt$p.value,2)))
 dev.off()
 
@@ -51,12 +51,13 @@ write.table(c(kt$method, ## kruskal.test name
               kt$statistic, ## X^2 from kruskal.test
               kt$parameter, ## df from kruskal.test
               round(kt$p.value,2), ## p-val from kruskal.test
-              max(a[,2]), ## total number of datasets
               sum(duplicated(a[,2])), ## number of datasets with >1 "best-fit"
+              max(a[,2]), ## total number of datasets
               ref[which(ref[,1]=="ve"),3], ## number of datasets called ve "best"
               ref[which(ref[,1]=="cu"),3], ## number of datasets called cu "best"
               AnB,
               ref[which(ref[,1]=="ba"),3], ## number of datasets called ba "best"
               ref[which(ref[,1]=="qu"),3], ## number of datasets called qu "best"
-              CnD
+              CnD,
+              ref[which(ref[,1]=="go"),3]
               ),file = "../data/ttt_stat.txt", quote = F, col.names = F, row.names = F)
