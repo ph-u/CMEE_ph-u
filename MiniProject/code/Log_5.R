08dcca56bf7b7ff5e5f7e484023a3527bdd31e2a
ph-u <51484056+ph-u@users.noreply.github.com>
2020-01-19 11:30:01 +0000

    random push of coursework after weeks

---
+++ MiniProject/code/Log_5.R
@@ -0,0 +1,20 @@
+#!/bin/env Rscript
+
+# Author: PokMan Ho pok.ho19@imperial.ac.uk
+# Script: Log_5.R
+# Desc: biological questions - do species correlate with model / parameter range?
+# Input: ```Rscript Log_5.R```
+# Output: statistical analysis report (.txt)
+# Arguments: 0
+# Date: Jan 2020
+
+## settings:
+## a = raw data (data subset in slave scripts)
+## a.~ = data.frames decending from raw data
+## f.~ = functions
+## v.~ = variables
+## i~ = temporary variables
+
+## library
+
+## data in
